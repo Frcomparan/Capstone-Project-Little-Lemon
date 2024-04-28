5af1ef076b4ef061033267c266a14d29b15e0fd6
Francisco Comparan <frcomparan@gmail.com>
2024-04-28 14:54:46 -0600

    chore: Create skeleton

---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <figure>
+        <img src='https://via.placeholder.com/150' alt='logo' />
+      </figure>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
